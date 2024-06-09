4d36260bb2e477c560096b3869ac8a9c7cf098ee
Ritik Kumar <ritikraj2018634@gmail.com>
2024-06-09 13:07:02 +0530

    stripe payment gateway completed

---
+++ app/_data/PricingPlan.jsx
@@ -0,0 +1,10 @@
+
+
+export default[
+    {
+        link:`${process.env.NEXT_PUBLIC_PAYMENT_LINK}`,
+        price:10.00,
+        priceId:`${process.env.NEXT_PUBLIC_PAYMENT_ID_LINK}`,
+        duration:'Every 3 months'
+    },
+]
